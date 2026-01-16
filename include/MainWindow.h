0d03b173d4404aa8b0e1472d9aae276f64dc297d
RohitPoul <poulrohit258@gmail.com>
2026-01-17 03:24:48 +0530

    idk if this app will work well or not but i need this most than anyone else rn. so here goes ntg

---
+++ include/MainWindow.h
@@ -0,0 +1,98 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QStatusBar>
+#include <QDockWidget>
+#include <QSlider>
+#include <QSpinBox>
+#include <QLabel>
+#include <QButtonGroup>
+#include <QRadioButton>
+#include <QPushButton>
+#include <QCloseEvent>
+#include <QProgressBar>
+
+class CanvasWidget;
+class ImageProcessor;
+class ToolManager;
+class HistoryManager;
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void openFile();
+    void quickExport();
+    void exportFile();
+    void resizeImage();
+    void undo();
+    void redo();
+    void zoomIn();
+    void zoomOut();
+    void fitToScreen();
+    void zoomToActual();
+    void toggleCompareOriginal();
+    void toggleSidebar();
+    void showShortcuts();
+    void onToolChanged(int toolId);
+    void onBackgroundChanged(int bgType);
+    void onZoomChanged(double zoom);
+    void updateStatusBar();
+
+protected:
+    void closeEvent(QCloseEvent* event) override;
+    void dragEnterEvent(QDragEnterEvent* event) override;
+    void dropEvent(QDropEvent* event) override;
+
+private:
+    void setupMenuBar();
+    void setupToolBar();
+    void setupToolPanel();
+    void setupStatusBar();
+    void setupShortcuts();
+    void connectSignals();
+    void loadImageFile(const QString& path);
+    bool confirmSaveBeforeClose();
+    QWidget* createSeparator();
+    void showProgress(bool show);
+
+    CanvasWidget* m_canvas;
+    ImageProcessor* m_processor;
+    ToolManager* m_toolManager;
+    HistoryManager* m_historyManager;
+
+    QDockWidget* m_toolDock;
+    QSlider* m_brushSizeSlider;
+    QSlider* m_toleranceSlider;
+    QSlider* m_softeningSlider;
+    QSlider* m_compareOpacitySlider;
+    QSpinBox* m_brushSizeSpin;
+    QSpinBox* m_toleranceSpin;
+    QSpinBox* m_softeningSpin;
+    QLabel* m_zoomLabel;
+    QLabel* m_imageSizeLabel;
+    QLabel* m_positionLabel;
+    QLabel* m_historyLabel;
+    QProgressBar* m_progressBar;
+    QButtonGroup* m_toolGroup;
+    QButtonGroup* m_bgGroup;
+    QPushButton* m_compareBtn;
+
+    QAction* m_undoAction;
+    QAction* m_redoAction;
+    QAction* m_compareAction;
+    QAction* m_toggleSidebarAction;
+    QAction* m_toggleSidebarBtn;
+    
+    bool m_isComparing = false;
+    QString m_currentFilePath;
+};
+
+#endif // MAINWINDOW_H
